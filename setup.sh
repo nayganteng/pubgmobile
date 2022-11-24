d8d86913b6c8e2b26988932d6891350532d46575
nayproject <115395427+nayganteng@users.noreply.github.com>
2022-11-24 18:46:47 +0700

    Shell ; setup instaling Inject

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/system/bin/sh
+
+#Package
+ST1=/data/data/com.termux/files/home
+PACKAGE=com.tencent.ig
+
+clear
+echo "\e[01;33m      ___                                    "
+sleep 1
+echo "     / _ | ___  ___  ___  __ ____ _  __ _____"
+sleep 1
+echo "    / __ |/ _ \/ _ \/ _ \/ // /  ' \/ // (_-<"
+sleep 1
+echo "   /_/ |_/_//_/\___/_//_/\_, /_/_/_/\_,_/___/"
+sleep 1
+echo "                       /___/         "
+sleep 1
+sleep 3
+echo "\e[01;33m𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐔𝐩𝐥𝐨𝐚𝐝𝐬 "
+echo
+echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█";
+sleep 3
+clear
+echo "\e[01;33m      ___                                    "
+sleep 1
+echo "     / _ | ___  ___  ___  __ ____ _  __ _____"
+sleep 1
+echo "    / __ |/ _ \/ _ \/ _ \/ // /  ' \/ // (_-<"
+sleep 1
+echo "   /_/ |_/_//_/\___/_//_/\_, /_/_/_/\_,_/___/"
+sleep 1
+echo "                       /___/         "
+sleep 1
+echo
+sleep 3
+echo "\e[01;33✨mᴇxᴛʀᴀᴄᴋ ꜰɪʟᴇꜱ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ"
+sleep 5
+echo
+if [ -f $ST1/pubgmobile/akenz.tar ]; then
+            sleep 4
+			tar -xvf $ST1/pubgmobile/akenz.tar
+			fi
+sleep 3
+echo "\e[01;33✨ɪɴꜱᴛᴀʟɪɴɢ ɪɴᴊᴇᴄᴛᴏʀ (200ᴋʙ)"
+if [ -f $ST1/pubgmobile/akenz.apk ]; then
+            sleep 4
+            pm install $ST1/pubgmobile/akenz.apk
+              fi
+sleep 3
+echo
